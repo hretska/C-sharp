2667c6efadbb5ba96a0ad220664564d11b8d828c
hretska <hretskauni@gmail.com>
2021-12-02 13:35:19 +0200

    lab 5

---
+++ lab 4/lab 4 register/Program.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace lab4_5____register
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var logins = File.ReadLines("Login.txt").ToList();
+            var passHash = File.ReadLines("Hash.txt").ToList();
+            var salt = File.ReadLines("Salt.txt").ToList();
+            bool valid = false;
+            string what;
+            while (!valid)
+            {
+                Console.WriteLine("Sign up - 1; Log in - 2");
+                what = Console.ReadLine();
+                switch (what)
+                {
+                    case "1":
+                        SignUp(logins, passHash, salt);
+                        valid = true;
+                        break;
+                    case "2":
+                        LogIn(logins, passHash, salt);
+                        valid = true;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Answer");
+                        valid = false;
+                        break;
+                }
+            }
+        }
+        public static void SignUp(List<string> logins, List<string> passHash, List<string> salt)
+        {
+            string YourLogin;
+            string YourPassword;
+            string RepeatPassword;
+            bool validLogin = false;
+            bool validPass = false;
+            while (!validLogin)
+            {
+                Console.WriteLine("Enter your Login");
+                YourLogin = Console.ReadLine();
+                if (logins.Contains(YourLogin))
+                {
+                    Console.WriteLine("That Login is already in use");
+                    validLogin = false;
+                }
+                else
+                {
+                    logins.Add(YourLogin);
+                    validLogin = true;
+                }
+            }
+            while (!validPass)
+            {
+                Console.WriteLine("Create a password");
+                YourPassword = Console.ReadLine();
+                var passSalt = PBKDF2.GenerateSalt();
+                salt.Add(Convert.ToBase64String(passSalt));
+                var pass = Encoding.Unicode.GetBytes(YourPassword);
+                var hashpass = PBKDF2.HashPassword(pass, passSalt, 80000);
+                passHash.Add(Convert.ToBase64String(hashpass));
+                Console.WriteLine("You have successfully signed up");
+                validPass = true;
+            }
+            File.WriteAllLines("Login.txt", logins);
+            File.WriteAllLines("Hash.txt", passHash);
+            File.WriteAllLines("Salt.txt", salt);
+
+        }
+        public static void LogIn(List<string> logins, List<string> passHash, List<string> salt)
+        {
+            string YourLogin;
+            string YourPassword;
+            int indLogin;
+            string passSalt = "";
+            string hashpass = "";
+            bool validLogin = false;
+            bool validPass = false;
+            while (!validLogin)
+            {
+                Console.WriteLine("Enter your Login");
+                YourLogin = Console.ReadLine();
+                if (logins.Contains(YourLogin))
+                {
+                    indLogin = logins.IndexOf(YourLogin);
+                    passSalt = salt[indLogin];
+                    hashpass = passHash[indLogin];
+                    validLogin = true;
+                }
+                else
+                {
+                    Console.WriteLine("User with this Login does not exist");
+                    validLogin = false;
+                }
+            }
+            while (!validPass)
+            {
+                Console.WriteLine("Enter your Password");
+                YourPassword = Console.ReadLine();
+                byte[] pass = Encoding.Unicode.GetBytes(YourPassword);
+                var hash = PBKDF2.HashPassword(pass, Convert.FromBase64String(passSalt), 80000);
+                if (Convert.ToBase64String(hash) == hashpass)
+                {
+                    Console.WriteLine("You have successfully Logged In");
+                    validPass = true;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Password");
+                    validPass = false;
+                }
+            }
+
+        }
+
+    }
+    public class PBKDF2
+    {
+        public static byte[] GenerateSalt()
+        {
+            const int saltLength = 32;
+            using (var randomNumberGenerator =
+            new RNGCryptoServiceProvider())
+            {
+                var randomNumber = new byte[saltLength];
+                randomNumberGenerator.GetBytes(randomNumber);
+                return randomNumber;
+            }
+        }
+        public static byte[] HashPassword(byte[] toBeHashed, byte[] salt, int numberOfRounds)
+        {
+            using (var rfc2898 = new Rfc2898DeriveBytes(
+            toBeHashed, salt, numberOfRounds))
+            {
+                return rfc2898.GetBytes(20);
+            }
+        }
+    }
+}
---
+++ lab 4/lab 4/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Diagnostics;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace lab4_5_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the password: ");
+            string password = Console.ReadLine();
+            byte[] salt = SaltedHash.GenerateSalt();
+            Console.WriteLine("Password: " + password);
+            Console.WriteLine("Salt = " + Convert.ToBase64String(salt));
+            Console.WriteLine();
+            var hashedPassword = SaltedHash.HashPasswordWithSalt(Encoding.UTF8.GetBytes(password), salt);
+            Console.WriteLine("Hashed Password = " + Convert.ToBase64String(hashedPassword));
+            Console.WriteLine();
+
+            
+            Console.WriteLine("Enter the password: ");
+            string password1 = Console.ReadLine();
+            int IterStart = 80000;
+            for (int i = 0; i < 10; i++)
+            {
+                var sw = new Stopwatch();
+                sw.Start();
+                byte[] salt1 = PBKDF2.GenerateSalt();
+                var hashedPassword1 = PBKDF2.HashPassword(Encoding.UTF8.GetBytes(password1), salt1, IterStart);
+                Console.WriteLine("Password: " + password1);
+                Console.WriteLine("Salt = " + Convert.ToBase64String(salt1));
+                Console.WriteLine("Hashed Password = " + Convert.ToBase64String(hashedPassword1));
+                Console.WriteLine("Iterations <" + IterStart + "> Elapsed Time: " + sw.ElapsedMilliseconds + "ms");
+                Console.WriteLine();
+                IterStart += 50000;
+            }
+
+
+        }
+
+    }
+    public class PBKDF2
+    {
+        public static byte[] GenerateSalt()
+        {
+            const int saltLength = 32;
+            using (var randomNumberGenerator =
+            new RNGCryptoServiceProvider())
+            {
+                var randomNumber = new byte[saltLength];
+                randomNumberGenerator.GetBytes(randomNumber);
+                return randomNumber;
+            }
+        }
+        public static byte[] HashPassword(byte[] toBeHashed, byte[] salt, int numberOfRounds)
+        {
+            using (var rfc2898 = new Rfc2898DeriveBytes(
+            toBeHashed, salt, numberOfRounds))
+            {
+                return rfc2898.GetBytes(20);
+            }
+        }
+    }
+
+    public class SaltedHash
+    {
+        public static byte[] GenerateSalt()
+        {
+            const int saltLength = 32;
+            using (var randomNumberGenerator =
+            new RNGCryptoServiceProvider())
+            {
+                var randomNumber = new byte[saltLength];
+                randomNumberGenerator.GetBytes(randomNumber);
+                return randomNumber;
+            }
+        }
+        private static byte[] Combine(byte[] pass, byte[] salt)
+        {
+            var ret = new byte[pass.Length + salt.Length];
+            Buffer.BlockCopy(pass, 0, ret, 0, pass.Length);
+            Buffer.BlockCopy(salt, 0, ret, pass.Length, salt.Length);
+            return ret;
+        }
+        public static byte[] HashPasswordWithSalt(
+        byte[] toBeHashed, byte[] salt)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                return sha256.ComputeHash(Combine(toBeHashed,
+                salt));
+            }
+        }
+    }
+}
